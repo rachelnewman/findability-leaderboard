38ed4dfe1029b0159d67e004781f5f17301fbeed
Rachel Newman <rachel.newman@argos.co.uk>
2020-06-19 16:19:06 +0100

    Updates

---
+++ precache-manifest.662d623cb9b356880820d07c843337ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff56bfdcf27ee02401ca9439aada92e2",
+    "url": "/findability-leaderboard/index.html"
+  },
+  {
+    "revision": "df64eaecc25b76344d45",
+    "url": "/findability-leaderboard/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4218ea6bfb1d31478440",
+    "url": "/findability-leaderboard/static/js/2.565c5ec8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/findability-leaderboard/static/js/2.565c5ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df64eaecc25b76344d45",
+    "url": "/findability-leaderboard/static/js/main.adeffea4.chunk.js"
+  },
+  {
+    "revision": "4ee4ce7845428ca72daa",
+    "url": "/findability-leaderboard/static/js/runtime-main.f562a267.js"
+  }
+]);
