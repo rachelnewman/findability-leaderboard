fee7a512754464266f7b1897f81f73df491dcb17
Rachel Newman <rachel.newman@argos.co.uk>
2020-06-19 16:05:33 +0100

    Build table with winners and things

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as Styled from './styles.js'
+import { Table } from './Table/Table.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Styled.Header className="App-header">
+				<h2>Findability Quiz Leaderboard</h2>
+      </Styled.Header>
+			<Table />
+
     </div>
   );
 }
---
+++ src/Table/styles.js
@@ -0,0 +1,45 @@
+import styled, { css } from "styled-components";
+
+export const Table = styled.table `
+margin: 20px auto;
+border-collapse: collapse;
+`
+export const TableSegment = styled.td `
+	padding: 5px 10px;
+	color: inherit;
+	background-color: inherit;
+	text-align: center;
+	${props => 
+	props.host && 
+	css `
+			background-color: purple;
+			color: white;
+			text-transform: uppercase;
+	`}
+
+	${props => 
+		props.NA && 
+		css `
+		opacity: 0.5;
+		`}
+
+`
+export const TableHeaderRow = styled.tr `
+	background-color: aliceblue;
+`
+
+export const TableRow = styled.tr `
+	border-top: 1px solid black;
+`
+export const TableBody = styled.tbody `
+	${TableRow}:nth-child(even) {
+		background-color: #e4e4e4;
+	}
+	${TableRow}:nth-child(odd) {
+		background-color: #c3c3c3;
+	}
+`
+
+export const WinnerText = styled.h1 `
+	text-align: center;
+`
